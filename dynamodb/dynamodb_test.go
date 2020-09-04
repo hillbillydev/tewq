ef2da8c5b11731c6b1267a84531397499bd4070b
Marcus Karlsson <marcus.caarlsson92@gmail.com>
2020-09-04 20:18:04 +1200

    WIP AddBasketItem

--- dynamodb/dynamodb_test.go
+++ dynamodb/dynamodb_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/uuid"
 	"github.com/matryer/is"
 )
 
@@ -195,7 +196,7 @@ func TestGetProductsByCategoryAndPrice(t *testing.T) {
 }
 
 func TestAddBasketItem(t *testing.T) {
-	customerId := NewSortableID()
+	customerId := uuid.New().String()
 	is := is.New(t)
 	product := Product{
 		Name:     "Golf Club",
@@ -224,73 +225,6 @@ func TestAddBasketItem(t *testing.T) {
 		ProductOptionID: o.ID,
 	})
 	is.NoErr(err)
-}
-
-func TestGetBasketProducts(t *testing.T) {
-	is := is.New(t)
-	products := []Product{
-		{
-			Name:     "Super Duper",
-			Category: "Clubs",
-			Options: []Option{
-				{
-					Color: "Green",
-					Stock: 2,
-				},
-			},
-		},
-		{
-			Name:     "A Shoe",
-			Category: "Shoes",
-			Options: []Option{
-				{
-					Color: "Brown",
-					Stock: 2,
-				},
-			},
-		},
-		{
-			Name:     "Adidas",
-			Category: "Shoes",
-			Options: []Option{
-				{
-					Color: "Red",
-					Stock: 3,
-				},
-			},
-		},
-	}
-
-	customerID := NewSortableID()
-	tdb, err := NewTestDynamoDB()
-	is.NoErr(err)
-	//defer tdb.Close()
-
-	for _, p := range products {
-		p, err := tdb.AddProduct(p)
-		is.NoErr(err)
-
-		o, err := tdb.AddOptionToProduct(p.ID, p.Options[0])
-		is.NoErr(err)
-
-		if p.Name == "A Shoe" {
-			continue
-		}
-
-		err = tdb.AddBasketItem(BasketItem{
-			CustomerID:      customerID,
-			ProductID:       p.ID,
-			ProductOptionID: o.ID,
-		})
-		is.NoErr(err)
-	}
-
-	p, err := tdb.GetBasketProducts(customerID)
-	is.NoErr(err)
-
-	is.True(len(p) == 2) // Only put 2 products in the basket..
-	is.True(p[0].Name != "A Shoe")
-	is.True(p[1].Name != "A Shoe")
 }
 
 type TestDynamoDB struct {
